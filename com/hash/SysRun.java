216ae3d5221274a79f96f5aedb4a14d983a02bb9
chenqun <chenqun@weidian.com>
2016-04-19 14:50:09 +0800

    b
    b

--- com/hash/SysRun.java
+++ com/hash/SysRun.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -21,9 +22,10 @@ import javax.swing.text.html.HTMLDocument.Iterator;
 public class SysRun {
 
 	String filename;
-	//public String[] hashmethods = {"RSHash", "JSHash", "ELFhash","PJWHash"};
-	public String[] hashmethods = {"RSHash", "JSHash", "ELFhash","PJWHash"};
+	public String[] hashmethods = {"RSHash", "APHash", "DJBHash","PJWHash"};
+	//public String[] hashmethods = {"APHash"};
 	public List<HashTable> tlist = new ArrayList<HashTable>();
+	public Set<String> checked = new HashSet<String>();
 	
 	public SysRun(String filename){
 		this.filename = filename;
@@ -52,33 +54,44 @@ public class SysRun {
 	public void query(String filename){
 		File input = new File(filename);
 		BufferedReader reader = null;
+		ExecutorService exs = Executors.newCachedThreadPool(); 
 		try{
 			reader = new BufferedReader(new FileReader(input));
 			String line = null;
 			while((line = reader.readLine()) != null){
 					String[] str = line.split("\t");
+					if(str.length < 2)
+						continue;
 					String doc_id = str[0];
+					//如果该doc 已经与其他的比较过，则放弃
+					if(checked.contains(doc_id))
+						continue;
 					String tokens = str[1];
-					List<String> result = new ArrayList<String>();
-					ExecutorService exs = Executors.newCachedThreadPool(); 
+					List<Future<Set<String>>> result = new ArrayList<Future<Set<String>>>();
+					
 					CountDownLatch countSignal = new CountDownLatch(hashmethods.length);
 					for(int i=0; i<this.hashmethods.length; i++){
 						try {
 							Future<Set<String>> f = exs.submit(new QueryThread(countSignal,tlist.get(i),hashmethods[i],tokens));
-							result.addAll(f.get());
+							result.add(f);
 						} catch (Exception e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						} 
 					}
-					try {
-						countSignal.await();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+//					try {
+//						countSignal.await();
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+					List<String> final_result = new ArrayList<String>();
+					for(Future<Set<String>> fs: result){
+						Set<String> tmp = fs.get();
+						final_result.addAll(tmp);
 					}
 					//result 去重复 4个hash函数中 有两个判断一样就返回
 					Map<String,Integer> map = new HashMap<String,Integer>();
-					for (String temp : result) {  
+					for (String temp : final_result) {  
 				           Integer count = map.get(temp);  
 				           map.put(temp, (count == null) ? 1 : count + 1); 
 				     }  
@@ -90,13 +103,16 @@ public class SysRun {
 						
 					}
 					map.remove(doc_id);
-					System.out.println(doc_id + "\t" + map.keySet());
-					exs.shutdown();
+					if(!map.isEmpty())
+						System.out.println(doc_id + "\t" + map.keySet());
+					checked.add(doc_id);
+					checked.addAll(map.keySet());
+					
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		
+		exs.shutdown();
 		
 	}
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
@@ -108,9 +124,7 @@ public class SysRun {
 //		}
 		String filename = "docs" ;
 		SysRun test = new SysRun(filename);
-	
 		test.buildTable(filename);
-		//System.out.println("build result " + test.tlist.get(0));
 		test.query("docs");
 		
 	}
